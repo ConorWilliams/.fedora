284629b162ed39be39061cd4430616807f499b75
conor williams <conorwilliams@outlook.com>
2024-09-08 12:19:53 +0100

    prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,16 @@ ssh-add ~/.ssh/github &>/dev/null
 # Disable the greeting
 function fish_greeting
 end
+
+
+function fish_prompt
+    set -l last_status $status
+    # Prompt status only if it's not 0
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status] "(set_color normal)
+    end
+
+    string join '' -- (set_color green) (prompt_pwd) (set_color normal) (fish_git_prompt) ' $ ' $stat
+end
+
